208d40750c5857df75877644437d5e6a42aac187
Ming Jhang Chen <ec1348@gmail.com>
2021-01-29 20:58:21 +0800

    Add funtion to let the user passing stock-code in the command line

--- StockPrice.js
+++ StockPrice.js
@@ -2,26 +2,29 @@
 //Solution: Use Node.js to connect to Taiwan Stock Exchange'API to get the stock price information to print out
 
 const https = require('https');
-const class_stockCode = "tse_2330"
 
 function printMessage(compony, stockPrice){
     const message = `Today, the price of ${compony} is ${stockPrice} NTD.`;
     console.log(message);
 }
 
-//Connect to the API URL (https://mis.twse.com.tw/stock/api/getStockInfo.jsp?ex_ch=${class_stockCode}.tw&json=1&delay=0&_=1611905929306)
-const request = https.get(`https://mis.twse.com.tw/stock/api/getStockInfo.jsp?ex_ch=${class_stockCode}.tw&json=1&delay=0&_=1611905929306`, response =>{
-    console.log(response.statusCode);
-    let body = "";
-    //Read the data 
-    response.on('data', data =>{
-        body +=  data.toString();
-    })
+function getProfile(class_stockCode){
+    //Connect to the API URL (https://mis.twse.com.tw/stock/api/getStockInfo.jsp?ex_ch=${class_stockCode}.tw&json=1&delay=0&_=1611905929306)
+    const request = https.get(`https://mis.twse.com.tw/stock/api/getStockInfo.jsp?ex_ch=${class_stockCode}.tw&json=1&delay=0&_=1611905929306`, response =>{
+        let body = "";
+        //Read the data 
+        response.on('data', data =>{
+            body +=  data.toString();
+        })
 
-    response.on('end', () => {
-        //Parse the data
-        const profile = JSON.parse(body);
-        //Print the data
-        printMessage(profile.msgArray.map(nf => nf.nf), profile.msgArray.map(z => z.z));
-    })
-});
+        response.on('end', () => {
+            //Parse the data
+            const profile = JSON.parse(body);
+            //Print the data
+            printMessage(profile.msgArray.map(nf => nf.nf),  profile.msgArray.map(z => z.z));
+        })
+    });
+}
+
+const getStockCode = process.argv.slice(2);
+getStockCode.forEach(getProfile);
