507fbd75df0f705141cf90943e22244fae823743
Ming Jhang Chen <ec1348@gmail.com>
2021-01-29 11:33:21 +0800

    First commit

---
+++ StockPrice.js
@@ -0,0 +1,8 @@
+//Problem: We need a simple way to get the stockPrice of a compony
+//Solution: Use Node.js to connect to Yahoo Finance'API to get the stock price information to print out
+
+
+//Connect to the API URL ()
+//Read the data 
+//Parse the data
+//Print the data
